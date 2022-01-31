eca20a2523cc495d8bc0cc860629566e27166f4a
Boronbaev Semetey <boronbaevsemetey@gmail.com>
2022-01-31 16:59:04 +0300

    present time

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const root = document.getElementById('root')
-const date = document.createElement('div')
-const time = document.createElement('div')
-date.textContent = "Today is : Friday"
-time.textContent = "Current time is : 4 PM : 50 : 22"
-root.appendChild(date)
-root.appendChild(time)
+const date = new Date();
+(function (){
+    const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const numWeek = date.getDay();
+    document.querySelector("#today").textContent = week[numWeek - 1]
+}());
+
+let sec = date.getSeconds();
+let min = date.getMinutes();
+let hour = date.getHours();
+
+function start(){
+    sec++;
+    if(sec === 60) {
+        sec = 0
+        min++
+    } else if(min === 60){
+        min = 0;
+        hour++;
+    } else if(hour === 24){
+        hour = 0
+    } else {
+        document.querySelector("#time").innerHTML = `${hour}:${min}:${sec}`
+    }
+}
+
+setInterval(start, 1000) 
