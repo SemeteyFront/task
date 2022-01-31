c6177ff0b1999fee9aba8714d493b1230b2b74cb
Boronbaev Semetey <boronbaevsemetey@gmail.com>
2022-01-31 16:57:26 +0300

    date and time

---
+++ script.js
@@ -0,0 +1,7 @@
+const root = document.getElementById('root')
+const date = document.createElement('div')
+const time = document.createElement('div')
+date.textContent = "Today is : Friday"
+time.textContent = "Current time is : 4 PM : 50 : 22"
+root.appendChild(date)
+root.appendChild(time)
